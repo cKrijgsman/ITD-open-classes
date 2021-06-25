ebe91f19cf8a868139956a7c4de2a8ca3b127af5
caspar krijgsman <c.j.krijgsman@student.tudelft.nl>
2021-06-25 15:24:42 +0200

    added a state-machine example

---
+++ P5-state-machine/sketch.js
@@ -0,0 +1,93 @@
+let currentState = "start";
+
+let currentKey = null;
+let currentKeyUsed = false;
+
+// Image variables
+let middleImage
+let leftImage
+let rightImage
+
+// Load all he images before we start the sketch.
+function preload() {
+  middleImage = loadImage("media/middle.png");
+  leftImage = loadImage("media/left.jpg");
+  rightImage = loadImage("media/right.jpg");
+}
+
+// Setup the initial values and canvas.
+function setup() {
+  createCanvas(1920, 1080);
+}
+
+function draw() {
+  // the background ensures that every frame is first cleared.
+  background(220);
+
+  // Execute the draw action of a state
+  visualizeStates();
+
+  // Check the transitions from a state
+  transitions();
+}
+
+function keyPressed() {
+  currentKey = key;
+  currentKeyUsed = false;
+}
+
+function visualizeStates() {
+  switch (currentState) {
+    case "start":
+      startState();
+      break
+    case "middle":
+      middleState()
+      break;
+    case "left":
+      leftState()
+      break;
+    case "right":
+      rightState()
+      break;
+  }
+}
+
+function transitions(){
+    switch (currentState) {
+      case "start": // Will move to middle once space is pressed.
+        if (currentKey === " " && !currentKeyUsed) {
+          currentKeyUsed = true;
+          currentState = "middle";
+        }
+        break;
+      case "middle": // Uses arrow left and right keys to navigate and space will return to start.
+        // Arrow navigation
+        if (currentKey === "ArrowLeft" && !currentKeyUsed) {
+          currentKeyUsed = true;
+          currentState = "left";
+        } else if (currentKey === "ArrowRight" && !currentKeyUsed){
+          currentKeyUsed = true;
+          currentState = "right";
+        }
+        // Return to start
+        if (currentKey === " " && !currentKeyUsed) {
+          currentKeyUsed = true;
+          currentState = "start";
+        }
+        break;
+      case "left": // Uses arrow back key to navigate back and space will return to start.
+      case "right": // Uses arrow back key to navigate back and space will return to start.
+        // Arrow navigation
+        if (currentKey === "ArrowDown" && !currentKeyUsed) {
+          currentKeyUsed = true;
+          currentState = "middle";
+        }
+        // Return to start
+        if (currentKey === " " && !currentKeyUsed) {
+          currentKeyUsed = true;
+          currentState = "start";
+        }
+        break;
+    }
+}
---
+++ P5-state-machine/states.js
@@ -0,0 +1,46 @@
+/**
+ * Visualization for the start state.
+ */
+function startState() {
+  push(); // Save canvas state
+
+  stroke(40);
+  textSize(32);
+  text("The Road to travel",450,420);
+
+  stroke(0);
+  textSize(16);
+  text("This is a simple example of a p5 state-machine.", 450, 470);
+  text("Press space to begin.", 450, 486);
+
+  pop(); // Restore canvas state
+}
+
+function leftState() {
+  push(); // Save canvas state
+
+  image(leftImage,0,0,1920,1080)
+
+  pop(); // Restore canvas state
+}
+
+function middleState(){
+  push(); // Save canvas state
+
+  stroke(0);
+  textSize(16);
+  text("A road less traveled they say.", 450, 280);
+  text("use the left or right arrow to move to the mountain or forrest", 450, 296);
+
+  image(middleImage, 278, 304);
+
+  pop(); // Restore canvas state
+}
+
+function rightState() {
+  push(); // Save canvas state
+
+  image(rightImage, 0, 0, 1920, 1080);
+
+  pop(); // Restore canvas state
+}
